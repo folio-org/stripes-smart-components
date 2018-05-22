b8c6b620d2f15048903ed5c112467eedc2154b15
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-05-22 15:47:10 -0500

    Move structures up a directory

---
+++ EditableItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ItemView from './ItemView';
+import ItemEdit from './ItemEdit';
+
+const EditableItem = props =>
+  (props.editing ? <ItemEdit {...props} autoFocus /> : <ItemView {...props} />);
+
+EditableItem.propTypes = {
+  editing: PropTypes.bool,
+};
+
+export default EditableItem;

d7ce9fab267069a2a5eebb69180f8aa1840cf25a
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-09-04 05:53:15 -0500

    Trim util directory

--- craftLayerUrl.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-
-function craftLayerUrl(mode) {
-  const url = this.props.location.pathname + this.props.location.search;
-  return _.includes(url, '?') ? `${url}&layer=${mode}` : `${url}?layer=${mode}`;
-}
-
-export default craftLayerUrl;
--- isStatefulComponent.js
+++
@@ -1,7 +0,0 @@
-const isStatefulComponent = component => typeof component.type !== 'string' && component.type.prototype.render;
-const isStatefulComponentType = componentType => typeof componentType !== 'string' && componentType.prototype.render;
-
-export {
-  isStatefulComponent,
-  isStatefulComponentType,
-};

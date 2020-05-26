f5735ef25ec0583893448ba5ef8cae2ecd26c4f2
Zak Burke <zburke@cornell.edu>
2020-05-26 09:49:47 -0400

    use canonical mapKeys instead of my own

--- tests/IntlHarness.js
+++ tests/IntlHarness.js
@@ -8,28 +8,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { IntlProvider } from 'react-intl';
+import { mapKeys } from 'lodash';
 import messages from '../translations/stripes-smart-components/en_US';
-
-/**
- * mimics the StripesTranslationPlugin in @folio/stripes-core:
- * given a list of key-value pairs like {"foo": "bar"} and a prefix,
- * return {"prefix.foo": "bar", ...}.
- *
- * @param {*} obj map of key-value pairs
- * @param {*} prefix module-path to prepend to each key
- */
-function prefixKeys(obj, prefix) {
-  const res = {};
-  for (const key of Object.keys(obj)) {
-    res[`${prefix}.${key}`] = obj[key];
-  }
-  return res;
-}
 
 const IntlHarness = ({ children }) => (
   <IntlProvider
     locale="en-US"
-    messages={prefixKeys(messages, 'stripes-smart-components')}
+    messages={mapKeys(messages, key => `stripes-smart-components.${key}`)}
   >
     {children}
   </IntlProvider>

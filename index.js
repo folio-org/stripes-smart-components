4a674aaa962f1ee6e5ccafee179f1e6c5e73c546
uladislausamets <vladislavsamec@gmail.com>
2021-02-19 14:40:03 +0300

    f

--- index.js
+++ index.js
@@ -69,3 +69,5 @@ export { default as ViewCustomFieldsSettings } from './lib/CustomFields/pages/Vi
 export { default as EditCustomFieldsSettings } from './lib/CustomFields/pages/EditCustomFieldsSettings';
 export { default as EditCustomFieldsRecord } from './lib/CustomFields/pages/EditCustomFieldsRecord';
 export { default as ViewCustomFieldsRecord } from './lib/CustomFields/pages/ViewCustomFieldsRecord';
+
+export * from './lib/utils';

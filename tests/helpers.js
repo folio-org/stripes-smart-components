915aba7dfb8200cb006a2d135464bf7d28855e9e
John Coburn <JCoburn@EBSCO.COM>
2022-06-10 13:30:57 -0500

    fewer beforEaches

--- tests/helpers.js
+++ tests/helpers.js
@@ -43,7 +43,7 @@ export function setupApplication({
   setupStripesCore({
     mirageOptions: {
       serverType: 'miragejs',
-      ...mirageOptions
+      ...mirageOptions,
     },
     scenarios,
     permissions,

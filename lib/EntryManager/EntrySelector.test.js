folio-org/stripes-smart-components

commit 1a8f5450b566c0cf21626760e2ef40de5e63f8c2

Perform case insensitive sorting when displaying locations, refs FOLIO-1416 (#555)

EntrySelector.js
@@ -42,7 +42,7 @@ class EntrySelector extends React.Component {
     paneTitle: PropTypes.string,
     paneWidth: PropTypes.string.isRequired,
     parentMutator: PropTypes.object.isRequired,
-    parentResources: PropTypes.object.isRequired,
+    parentResources: PropTypes.object,
     resources: PropTypes.object.isRequired,
     stripes: PropTypes.object,
   };
@@ -131,7 +131,7 @@ class EntrySelector extends React.Component {
       paneSetWidth
     } = this.props;
 
-    const links = _.sortBy(contentData, [nameKey]).map(item => (
+    const links = _.sortBy(contentData, [record => record[nameKey].toLowerCase()]).map(item => (
       <NavListItem
         key={item.id}
         to={this.linkPath(item.id)}

commit 81815d3dd9aeba64c0ce06fd2020741450b3db33

STSMACOM-893 EntryManager sort by id if sort-key is absent (#1561)
Avoid an NPE when sorting lists if the requested sort-key is missing by
using `id` as the key instead. Should we check for `id`? Eh, maybe, but
`<EntryManager>` is a tool for handling entries retrieved from API
requests so it seems like a safe assumption.

This bug was introduced in 1a8f5450b566c0cf21626760e2ef40de5e63f8c2.

Refs STSMACOM-893

lib/EntryManager/EntrySelector.js
@@ -22,6 +22,19 @@ import {
 } from '@folio/stripes-components';
 import ConnectedWrapper from './ConnectedWrapper';
 
+/**
+ * listSortByLowerCase
+ * Sort the list by the lower-cased value of key. If the key is not present
+ * on an item, use its `id` property instead.
+ *
+ * @param {Array} list Array of items to sort
+ * @param {string} key property of an object to sort by
+ * @returns list, sorted by the lower-cased value of key
+ */
+export const listSortByLowerCase = (list, key) => {
+  return _.sortBy(list, [(i) => (i[key] ? i[key].toLowerCase() : i.id)]);
+};
+
 class EntrySelector extends React.Component {
   static propTypes = {
     addButtonTitle: PropTypes.string,
@@ -331,7 +344,7 @@ class EntrySelector extends React.Component {
     } = this.props;
 
     const rows = this.filteredRows(contentData);
-    const links = _.sortBy(rows, [record => record[nameKey].toLowerCase()]).map(item => (
+    const links = listSortByLowerCase(rows, nameKey).map(item => (
       <div data-test-nav-list-item key={item.id}>
         <NavListItem
           to={this.linkPath(item.id)}

lib/EntryManager/EntrySelector.test.js
@@ -1,0 +1,27 @@
+import { listSortByLowerCase } from './EntrySelector';
+
+describe('listSortByLowerCase', () => {
+  it('sorts by [key] when key is present', () => {
+    const list = [
+      { id: 'AA', name: 'Aa' },
+      { id: 'Ab', name: 'Ab' },
+      { id: 'b', name: 'b' },
+      { id: 'c', name: 'C' },
+    ];
+
+    const shuffled = [...list].sort(() => Math.random() - 0.5);
+    expect(listSortByLowerCase(shuffled, 'name')).toEqual(list);
+  });
+
+  it('sorts by [id] when key is missing', () => {
+    const list = [
+      { id: 'Aa' },
+      { id: 'Ab', name: 'Ab' },
+      { id: 'b', name: 'b' },
+      { id: 'c', name: 'C' },
+    ];
+
+    const shuffled = [...list].sort(() => Math.random() - 0.5);
+    expect(listSortByLowerCase(shuffled, 'name')).toEqual(list);
+  });
+});
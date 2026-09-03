917e1d89fed1e9c082c1b31ed41c6008646c7e29
Dmitriy-Litvinenko <Dmitriy_Litvinenko@epam.com>
2026-09-03 22:44:28 +0300

    STSMACOM-963: Fix false Field is required warning on non-required multiselect (#1643)

--- tests/jest/__mock__/reactFinalFormListeners.mock.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-jest.mock('react-final-form-listeners', () => {
-  return {
-    ...jest.requireActual('react-final-form-listeners'),
-
-    OnChange: jest.fn(({ children }) => <>{children()}</>),
-  };
-});
